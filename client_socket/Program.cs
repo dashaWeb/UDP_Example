701964d6a3dd5b57fc2b75684cea46dfc3c09d35
Dasha <dashakonopelko@gmail.com>
2024-04-15 20:02:42 +0300

    Add project files.

---
+++ client_socket/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+internal class Program
+{
+    static string address = "127.0.0.1";
+    static int port = 8080; // 1000 ... 60 000
+    private static void Main(string[] args)
+    {
+		try
+		{
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+            IPEndPoint remotePoint = new IPEndPoint(IPAddress.Any, 0);
+            //Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            string message = "";
+            UdpClient client = new UdpClient();
+            while (message != "end")
+            {
+                Console.Write("Enter a message :: ");
+                message = Console.ReadLine();
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                //socket.SendTo(data, ipPoint);
+                client.Send(data,data.Length,ipPoint);
+
+                int bytes = 0;
+                string response = "";
+                data = new byte[1024];
+
+                /*do
+                {
+                    bytes = socket.ReceiveFrom(data, ref remotePoint);
+                    response += Encoding.Unicode.GetString(data);
+
+                } while (socket.Available > 0);*/
+                data = client.Receive(ref remotePoint);
+                response = Encoding.Unicode.GetString(data);
+                Console.WriteLine("server response :: " + response);
+            }
+            /*socket.Shutdown(SocketShutdown.Both);
+            socket.Close();*/
+            client.Close();
+        }
+		catch (Exception ex)
+		{
+
+            Console.WriteLine(ex.Message);
+        }
+        
+    }
+}
---
+++ server_socket/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+internal class Program
+
+{
+    static string address = "127.0.0.1"; // поточний адрес
+    static int port = 8080; // порт для отримання вхідних запитів
+    private static void Main(string[] args)
+    {
+        IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+        IPEndPoint remotePoint = new IPEndPoint(IPAddress.Any, 0);
+
+        //Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        UdpClient listener = new UdpClient(ipPoint);
+        try
+        {
+            //listenSocket.Bind(ipPoint);
+            Console.WriteLine("Server started! Waiting for connection .... ");
+
+            while (true)
+            {
+                int bytes = 0;
+                /*byte[] data = new byte[1024];
+
+                bytes = listenSocket.ReceiveFrom(data, ref remotePoint);*/
+                byte[] data = listener.Receive(ref remotePoint);
+                string msg = Encoding.Unicode.GetString(data);
+                Console.WriteLine($"{DateTime.Now.ToShortDateString()} :: {msg}");
+
+
+                string message = "Message was send";
+                data = Encoding.Unicode.GetBytes(message);
+                listener.Send(data,data.Length, remotePoint);
+                //listenSocket.SendTo(data, remotePoint);
+
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        listener.Close();
+    }
+}
